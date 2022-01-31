7b8f85090ef1ed275ebba7f8f05ecd2d19ba7c5a
Prokop Simek <prokop@dxheroes.io>
2022-01-31 20:09:57 +0100

    refactor: sections as object

--- src/components/BlogPostsSection.js
+++ src/components/BlogPostsSection.js
@@ -2,58 +2,53 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 const BlogPostsSection = ({ type, text }) => {
-  let renderSection;
 
-  switch (type) {
-    case "blog":
-      renderSection = (
-        <div className="section-title text-center">
-          <h2>All Blog Posts</h2>
-          <ul className="breadcrumb-nav">
-            <li><Link to="/">Home</Link></li>
-            <li>All blog posts</li>
-          </ul>
-        </div>
-      )
-      break;
-    case "category":
-      renderSection = (
-        <div className="section-title text-center">
-          <h2>Blog Posts by Category</h2>
-          <ul className="breadcrumb-nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li>Category: {text}</li>
-          </ul>
-        </div>
-      )
-      break;
-    case "tag":
-      renderSection = (
-        <div className="section-title text-center">
-          <h2>Blog Posts by Tag</h2>
-          <ul className="breadcrumb-nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li>Tag: {text}</li>
-          </ul>
-        </div>
-      )
-      break;
-    case "search":
-      renderSection = (
-        <div className="section-title text-center">
-          <h2>Search Results</h2>
-          <ul className="breadcrumb-nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li>Search: {text}</li>
-          </ul>
-        </div>
-      )
-      break;
-    default:
-      throw new Error("Invalid type for blog post section");
+  const sections = {
+    "blog": (
+      <div className="section-title text-center">
+        <h2>All Blog Posts</h2>
+        <ul className="breadcrumb-nav">
+          <li><Link to="/">Home</Link></li>
+          <li>All blog posts</li>
+        </ul>
+      </div>
+    ),
+    "category": (
+      <div className="section-title text-center">
+        <h2>Blog Posts by Category</h2>
+        <ul className="breadcrumb-nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li>Category: {text}</li>
+        </ul>
+      </div>
+    ),
+    "tag": (
+      <div className="section-title text-center">
+        <h2>Blog Posts by Tag</h2>
+        <ul className="breadcrumb-nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li>Tag: {text}</li>
+        </ul>
+      </div>
+    ),
+    "search": (
+      <div className="section-title text-center">
+        <h2>Search Results</h2>
+        <ul className="breadcrumb-nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li>Search: {text}</li>
+        </ul>
+      </div>
+    )
+  }
+
+  const renderSection = sections[type]
+
+  if (!renderSection) {
+    throw new Error("Invalid type for blog post section");
   }
 
   return (
